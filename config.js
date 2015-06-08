b3548ecfc5c99da17abcc08cbc5143ea81e2ffe0
Volkhard Engelstädter <ve@vivahome.de>
2015-06-08 14:40:45 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 
 var config = {};
 
-config.webPort = 8000;
+config.webPort = 8050;
 
 config.serialBaudRate = 115200;
 
 // expects a webcam stream from mjpg_streamer
-config.webcamPort = 8080;
+config.webcamPort = 8090;
 
 module.exports = config;
